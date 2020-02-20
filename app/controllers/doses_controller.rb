ad3603729076fc203fe4c0e62863c7bd5c5c6a06
Pawel Maryon <maryonpawel@gmail.com>
2020-02-20 16:47:50 +0000

    doses form

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,21 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save 
+      redirect_to @dose
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name, :description, :ingredient)
+  end
 end
